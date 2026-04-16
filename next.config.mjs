4739c8136809c76b9ee2166490cd486964b5bc21
Muaaz8 <111640774+Muaaz8@users.noreply.github.com>
2026-04-16 20:29:58 +0500

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true, // 👈 IMPORTANT for cPanel hosting
+
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'spp.statelyweb.co.uk',
-        port: '',
-        pathname: '/admin/uploads/user_images/**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
